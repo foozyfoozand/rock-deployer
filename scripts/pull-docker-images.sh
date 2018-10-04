c191d7ce7223434f574cdd66482c869ca8dd9ac5
Prodian <micah.downing@gmail.com>
2018-10-04 08:36:14 -0500

    THISISCVAH-1093

---
+++ scripts/pull-docker-images.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+
+pushd "/opt" > /dev/null
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root or use sudo."
+  exit
+fi
+
+function run_cmd {
+	local command="$@"
+	eval $command
+	local ret_val=$?
+	if [ $ret_val -ne 0 ]; then
+		echo "$command returned error code $ret_val"
+        exit 1
+	fi	
+}
+
+function use_laprepos() {
+    if [ -z "$TFPLENUM_LABREPO" ]; then
+        echo "Select a docker registry to use:"
+        select cr in "docker.labrepo.lan" "docker.io"; do
+            case $cr in
+                docker.labrepo.lan ) export TFPLENUM_LABREPO=true; break;;
+                docker.io ) export TFPLENUM_LABREPO=false; break;;
+            esac
+        done
+    fi
+}
+
+function execute_playbook(){
+    pushd "/opt/tfplenum-deployer/playbooks" > /dev/null
+    local os_id=$(awk -F= '/^ID=/{print $2}' /etc/os-release)
+    if [ "$os_id" == '"centos"' ]; then
+        export TFPLENUM_OS_TYPE=Centos
+    else
+        export TFPLENUM_OS_TYPE=RedHat
+    fi 
+
+    make pull-docker-images
+    popd > /dev/null
+}
+
+use_laprepos
+execute_playbook
